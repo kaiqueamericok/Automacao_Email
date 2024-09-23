17a3c90d6c9cb9721cbe1f78a5a2b62c5e6b48b5
kaiqueamericok <101136374+kaiqueamericok@users.noreply.github.com>
2024-09-23 10:48:07 -0300

    Add files via upload

---
+++ 4-automacao_email_acoes/email_bb.py
@@ -0,0 +1,41 @@
+from email.message import EmailMessage
+import smtplib
+import ssl
+import mimetypes
+
+password = open('senha', 'r').read()
+from_email = 'kaiquetecnologia5@gmail.com'
+to_email = 'kaiquemathor@gmail.com, kaiquetecnologia5@gmail.com'
+subject = 'Informes BB'
+body = open('dados/corpo.txt', 'r', encoding='utf-8').read()
+
+# 1 - Montando estrtura do e-mail
+message = EmailMessage()
+message['From'] = from_email
+message['To'] = to_email
+message['Subject'] = subject
+
+message.set_content(body)
+safe = ssl.create_default_context()
+
+# 2 - Adicionar Anexo
+
+anexo = 'bb_preco.png'
+#print(mimetypes.guess_type(anexo)[0])
+mime_type, mime_subtype = mimetypes.guess_type(anexo)[0].split('/')
+with open(anexo, 'rb') as a:
+    message.add_attachment(
+        a.read(),
+        maintype=mime_type,
+        subtype=mime_subtype,
+        filename=anexo
+    )
+
+# 3 - Envio de e-mail
+with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=safe) as smtp:
+    smtp.login(from_email, password)
+    smtp.sendmail(
+        from_email,
+        to_email,
+        message.as_string()
+    )
