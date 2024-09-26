608a6af033e684580f0d28b085b07056c59cc7fe
kaiqueamericok <101136374+kaiqueamericok@users.noreply.github.com>
2024-09-26 19:45:47 -0300

    Update email_auto.py

--- 1-Automacao_Email_Simples/email_auto.py
+++ 1-Automacao_Email_Simples/email_auto.py
@@ -4,8 +4,8 @@ import ssl
 
 password = open('senha', 'r').read()
 #print(password)
-from_email = 'kaiquetecnologia5@gmail.com'
-to_email = 'kaiquemathor@gmail.com , kaiquetecnologia5@gmail.com'
+from_email = 'seu-email@gmail.com'
+to_email = 'destino@gmail.com , destino2@gmail.com'
 subject = 'Consultoria T.I'
 body = '''
 Prezados,
