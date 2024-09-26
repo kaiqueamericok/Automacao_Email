24442e645d0a52d65da58c86155963395d157e7e
kaiqueamericok <101136374+kaiqueamericok@users.noreply.github.com>
2024-09-26 19:47:28 -0300

    Update email_bb.py

--- 4-automacao_email_acoes/email_bb.py
+++ 4-automacao_email_acoes/email_bb.py
@@ -4,8 +4,8 @@ import ssl
 import mimetypes
 
 password = open('senha', 'r').read()
-from_email = 'kaiquetecnologia5@gmail.com'
-to_email = 'kaiquemathor@gmail.com, kaiquetecnologia5@gmail.com'
+from_email = 'seu-email@gmail.com'
+to_email = 'destino@gmail.com , destino2@gmail.com'
 subject = 'Informes BB'
 body = open('dados/corpo.txt', 'r', encoding='utf-8').read()
 
